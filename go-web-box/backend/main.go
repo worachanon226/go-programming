67e6d6e57a943eb5d31f3dc183ea3ec996b0f163
Amaz <worachanon226@hotmail.com>
2022-07-27 19:36:39 +0700

    update client

--- go-web-box/backend/main.go
+++ go-web-box/backend/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"sort"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -18,6 +19,15 @@ func main() {
 
 	lists := []List{}
 
+	app.Patch("/update", func(c *fiber.Ctx) error {
+
+		sort.Slice(lists, func(i, j int) bool {
+			return lists[i].Title < lists[j].Title
+		})
+
+		return c.JSON(lists)
+	})
+
 	app.Post("/addlists", func(c *fiber.Ctx) error {
 
 		list := &List{}
@@ -29,6 +39,28 @@ func main() {
 		list.ID = len(lists) + 1
 		lists = append(lists, *list)
 
+		return c.JSON(list)
+	})
+
+	app.Get("/get", func(c *fiber.Ctx) error {
+		return c.JSON(lists)
+	})
+
+	app.Get("/get/:id", func(c *fiber.Ctx) error {
+
+		id, err := c.ParamsInt("id")
+
+		if err != nil {
+			return c.Status(401).SendString("Invalid ID")
+		}
+
+		var list List
+
+		for i, t := range lists {
+			if t.ID == id {
+				list = lists[i]
+			}
+		}
 		return c.JSON(list)
 	})
 
