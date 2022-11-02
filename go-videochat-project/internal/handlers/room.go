5e0d8e65d99fa7c0f8e47ad6cb87e8c3a0c3da66
Amaz <worachanon226@hotmail.com>
2022-11-02 13:58:09 +0700

    Update go-videochat-project

--- go-videochat-project/internal/handlers/room.go
+++ go-videochat-project/internal/handlers/room.go
@@ -1,1 +1,36 @@
+package handlers
 
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/websocket/v2"
+	guuid "github.com/google/uuid"
+)
+
+func RoomCreate(c *fiber.Ctx) error {
+	return c.Redirect(fmt.Sprintf("/room/%s", guuid.New().String()))
+}
+
+func Room(c *fiber.Ctx) error {
+	uuid := c.Params("uuid")
+	if uuid == "" {
+		c.Status(400)
+		return nil
+	}
+
+	uuid, suuid, _ := createOrGetRoom(uuid)
+}
+
+func RoomChatWebSocket(c *websocket.Conn) {
+	uuid := c.Params("uuid")
+	if uuid == "" {
+		return
+	}
+
+	_, _, room := createOrGetRoom(uuid)
+}
+
+func createOrGetRoom(uuid string) (string, string, Room) {
+
+}
